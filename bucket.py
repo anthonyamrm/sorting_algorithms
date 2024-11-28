af9c6032fdbc3dcd6fdf333d13d742171c6b57c3
Anthony Rios Amorim <anthonyamorim02@gmail.com>
2024-11-27 21:51:00 -0300

    Add files via upload
    resultado bucket

---
+++ bucket.py
@@ -0,0 +1,390 @@
+import time
+import csv
+import os
+import random
+import statistics
+
+def gerar_lista_aleatoria(tamanho):
+    return random.sample(range(tamanho * 10), tamanho)
+
+def calcular_desvio_padrao(tempos):
+    return statistics.stdev(tempos)
+
+def insertion_sort(bucket):
+    for i in range (1, len (bucket)):
+        var = bucket[i]
+        j = i - 1
+        while (j >= 0 and var < bucket[j]):
+            bucket[j + 1] = bucket[j]
+            j = j - 1
+        bucket[j + 1] = var
+
+def bucketSort(input_list):
+    # Find maximum value in the list and use length of the list to determine which value in the list goes into which bucket 
+    max_value = max(input_list)
+    size = max_value/len(input_list)
+
+    # Create n empty buckets where n is equal to the length of the input list
+    buckets_list= []
+    for x in range(len(input_list)):
+        buckets_list.append([]) 
+
+    # Put list elements into different buckets based on the size
+    for i in range(len(input_list)):
+        j = int (input_list[i] / size)
+        if j != len (input_list):
+            buckets_list[j].append(input_list[i])
+        else:
+            buckets_list[len(input_list) - 1].append(input_list[i])
+
+    # Sort elements within the buckets using Insertion Sort
+    for z in range(len(input_list)):
+        insertion_sort(buckets_list[z])
+            
+    # Concatenate buckets with sorted elements into a single list
+    final_output = []
+    for x in range(len (input_list)):
+        final_output = final_output + buckets_list[x]
+    return final_output
+
+def main1():
+
+
+    tempo_bucket = []
+    aux = 0
+    soma = 0
+    sorted_list = []
+
+    # Rodar o código 5 vezes, mas considerar apenas as últimas 4 execuções
+    while aux != 5:
+        vetor_500 = gerar_lista_aleatoria(500)
+        print(vetor_500[:10])
+        copy = vetor_500.copy()
+
+        # Começa a contagem do tempo
+        start = time.perf_counter()
+        sorted_list = bucketSort(copy)
+        end = time.perf_counter()
+
+        # Adicionar o tempo de execução apenas se não for a primeira iteração
+        
+        tempo_bucket.append(1000*(end - start))
+
+
+        aux += 1
+
+    print('SORTED LIST (última execução):')
+    print(sorted_list)
+
+    # Calculando a média do tempo de execução considerando apenas as 4 últimas iterações
+    soma = sum(tempo_bucket)
+    media = soma / len(tempo_bucket)
+    desvio = calcular_desvio_padrao(tempo_bucket)
+
+    # Escrevendo os tempos de execução no arquivo CSV
+    with open("bucket_500.csv", "w", newline="", encoding="utf-8") as aarquivo:
+        escritor = csv.writer(aarquivo)
+        escritor.writerow(["bucket_500: "])  # Cabeçalho
+        for valor in tempo_bucket:
+            escritor.writerow([valor])
+        escritor.writerow(["Média do tempo de execução:", media])
+        escritor.writerow(["Desvio padrão do tempo de execução:", desvio])
+
+def main2():
+    
+
+    tempo_bucket = []
+    aux = 0
+    soma = 0
+    sorted_list = []
+
+    # Rodar o código 5 vezes, mas considerar apenas as últimas 4 execuções
+    while aux != 5:
+        vetor_1000 = gerar_lista_aleatoria(1000)
+        print(vetor_1000[:10])
+        copy = vetor_1000.copy()
+
+        # Começa a contagem do tempo
+        start = time.perf_counter()
+        sorted_list = bucketSort(copy)
+        end = time.perf_counter()
+
+        # Adicionar o tempo de execução apenas se não for a primeira iteração
+        
+        tempo_bucket.append(1000*(end - start))
+
+        aux += 1
+
+    print('SORTED LIST (última execução):')
+    print(sorted_list)
+
+    # Calculando a média do tempo de execução considerando apenas as 4 últimas iterações
+    soma = sum(tempo_bucket)
+    media = soma / len(tempo_bucket)
+    desvio = calcular_desvio_padrao(tempo_bucket)
+
+    # Escrevendo os tempos de execução no arquivo CSV
+    with open("bucket_1000.csv", "w", newline="", encoding="utf-8") as aarquivo:
+        escritor = csv.writer(aarquivo)
+        escritor.writerow(["bucket_1000: "])  # Cabeçalho
+        for valor in tempo_bucket:
+            escritor.writerow([valor])
+        escritor.writerow(["Média do tempo de execução:", media])
+        escritor.writerow(["Desvio padrão do tempo de execução:", desvio])
+
+def main3():
+
+    tempo_bucket = []
+    aux = 0
+    soma = 0
+    sorted_list = []
+
+    # Rodar o código 5 vezes, mas considerar apenas as últimas 4 execuções
+    while aux != 5:
+        vetor_5000 = gerar_lista_aleatoria(5000)
+        print(vetor_5000[:10])
+        copy = vetor_5000.copy()
+
+        # Começa a contagem do tempo
+        start = time.perf_counter()
+        sorted_list = bucketSort(copy)
+        end = time.perf_counter()
+
+        # Adicionar o tempo de execução apenas se não for a primeira iteração
+        
+        tempo_bucket.append(1000*(end - start))
+
+        aux += 1
+
+    print('SORTED LIST (última execução):')
+    print(sorted_list)
+
+    # Calculando a média do tempo de execução considerando apenas as 4 últimas iterações
+    soma = sum(tempo_bucket)
+    media = soma / len(tempo_bucket)
+    desvio = calcular_desvio_padrao(tempo_bucket)
+
+    # Escrevendo os tempos de execução no arquivo CSV
+    with open("bucket_5000.csv", "w", newline="", encoding="utf-8") as aarquivo:
+        escritor = csv.writer(aarquivo)
+        escritor.writerow(["bucket_5000: "])  # Cabeçalho
+        for valor in tempo_bucket:
+            escritor.writerow([valor])
+        escritor.writerow(["Média do tempo de execução:", media])
+        escritor.writerow(["Desvio padrão do tempo de execução:", desvio])
+
+def main4():
+
+    tempo_bucket = []
+    aux = 0
+    soma = 0
+    sorted_list = []
+
+    # Rodar o código 5 vezes, mas considerar apenas as últimas 4 execuções
+    while aux != 5:
+        vetor_30000 = gerar_lista_aleatoria(30000)
+        print(vetor_30000[:10])
+        copy = vetor_30000.copy()
+
+        # Começa a contagem do tempo
+        start = time.perf_counter()
+        sorted_list = bucketSort(copy)
+        end = time.perf_counter()
+
+        # Adicionar o tempo de execução apenas se não for a primeira iteração
+        
+        tempo_bucket.append(1000*(end - start))
+
+        aux += 1
+
+    print('SORTED LIST (última execução):')
+    print(sorted_list)
+
+    # Calculando a média do tempo de execução considerando apenas as 4 últimas iterações
+    soma = sum(tempo_bucket)
+    media = soma / len(tempo_bucket)
+    desvio = calcular_desvio_padrao(tempo_bucket)
+
+    # Escrevendo os tempos de execução no arquivo CSV
+    with open("bucket_30000.csv", "w", newline="", encoding="utf-8") as aarquivo:
+        escritor = csv.writer(aarquivo)
+        escritor.writerow(["bucket_30000: "])  # Cabeçalho
+        for valor in tempo_bucket:
+            escritor.writerow([valor])
+        escritor.writerow(["Média do tempo de execução:", media])
+        escritor.writerow(["Desvio padrão do tempo de execução:", desvio])
+
+def main5():
+    
+
+    tempo_bucket = []
+    aux = 0
+    soma = 0
+    sorted_list = []
+
+    # Rodar o código 5 vezes, mas considerar apenas as últimas 4 execuções
+    while aux != 5:
+        vetor_80000 = gerar_lista_aleatoria(80000)
+        print(vetor_80000[:10])
+        copy = vetor_80000.copy()
+
+        # Começa a contagem do tempo
+        start = time.perf_counter()
+        sorted_list = bucketSort(copy)
+        end = time.perf_counter()
+
+        # Adicionar o tempo de execução apenas se não for a primeira iteração
+        
+        tempo_bucket.append(1000*(end - start))
+
+        aux += 1
+
+    print('SORTED LIST (última execução):')
+    print(sorted_list)
+
+    # Calculando a média do tempo de execução considerando apenas as 4 últimas iterações
+    soma = sum(tempo_bucket)
+    media = soma / len(tempo_bucket)
+    desvio = calcular_desvio_padrao(tempo_bucket)
+
+    # Escrevendo os tempos de execução no arquivo CSV
+    with open("bucket_80000.csv", "w", newline="", encoding="utf-8") as aarquivo:
+        escritor = csv.writer(aarquivo)
+        escritor.writerow(["bucket_80000: "])  # Cabeçalho
+        for valor in tempo_bucket:
+            escritor.writerow([valor])
+        escritor.writerow(["Média do tempo de execução:", media])
+        escritor.writerow(["Desvio padrão do tempo de execução:", desvio])
+
+def main6():
+
+    tempo_bucket = []
+    aux = 0
+    soma = 0
+    sorted_list = []
+
+    # Rodar o código 5 vezes, mas considerar apenas as últimas 4 execuções
+    while aux != 5:
+        vetor_100000 = gerar_lista_aleatoria(100000)
+        print(vetor_100000[:10])
+        copy = vetor_100000.copy()
+
+        # Começa a contagem do tempo
+        start = time.perf_counter()
+        sorted_list = bucketSort(copy)
+        end = time.perf_counter()
+
+        # Adicionar o tempo de execução apenas se não for a primeira iteração
+        
+        tempo_bucket.append(1000*(end - start))
+
+        aux += 1
+
+    print('SORTED LIST (última execução):')
+    print(sorted_list)
+
+    # Calculando a média do tempo de execução considerando apenas as 4 últimas iterações
+    soma = sum(tempo_bucket)
+    media = soma / len(tempo_bucket)
+    desvio = calcular_desvio_padrao(tempo_bucket)
+
+    # Escrevendo os tempos de execução no arquivo CSV
+    with open("bucket_100000.csv", "w", newline="", encoding="utf-8") as aarquivo:
+        escritor = csv.writer(aarquivo)
+        escritor.writerow(["bucket_100000: "])  # Cabeçalho
+        for valor in tempo_bucket:
+            escritor.writerow([valor])
+        escritor.writerow(["Média do tempo de execução:", media])
+        escritor.writerow(["Desvio padrão do tempo de execução:", desvio])
+
+def main7():
+
+    tempo_bucket = []
+    aux = 0
+    soma = 0
+    sorted_list = []
+
+    # Rodar o código 5 vezes, mas considerar apenas as últimas 4 execuções
+    while aux != 5:
+        vetor_150000 = gerar_lista_aleatoria(150000)
+        print(vetor_150000[:10])
+        copy = vetor_150000.copy()
+
+        # Começa a contagem do tempo
+        start = time.perf_counter()
+        sorted_list = bucketSort(copy)
+        end = time.perf_counter()
+
+        # Adicionar o tempo de execução apenas se não for a primeira iteração
+        
+        tempo_bucket.append(1000*(end - start))
+
+        aux += 1
+
+    print('SORTED LIST (última execução):')
+    print(sorted_list)
+
+    # Calculando a média do tempo de execução considerando apenas as 4 últimas iterações
+    soma = sum(tempo_bucket)
+    media = soma / len(tempo_bucket)
+    desvio = calcular_desvio_padrao(tempo_bucket)
+
+
+    # Escrevendo os tempos de execução no arquivo CSV
+    with open("bucket_150000.csv", "w", newline="", encoding="utf-8") as aarquivo:
+        escritor = csv.writer(aarquivo)
+        escritor.writerow(["bucket_150000: "])  # Cabeçalho
+        for valor in tempo_bucket:
+            escritor.writerow([valor])
+        escritor.writerow(["Média do tempo de execução:", media])
+        escritor.writerow(["Desvio padrão do tempo de execução:", desvio])
+
+def main8():
+
+    tempo_bucket = []
+    aux = 0
+    soma = 0
+    sorted_list = []
+
+    # Rodar o código 5 vezes, mas considerar apenas as últimas 4 execuções
+    while aux != 5:
+        vetor_200000 = gerar_lista_aleatoria(200000)
+        print(vetor_200000[:10])
+        copy = vetor_200000.copy()
+
+        # Começa a contagem do tempo
+        start = time.perf_counter()
+        sorted_list = bucketSort(copy)
+        end = time.perf_counter()
+
+        # Adicionar o tempo de execução apenas se não for a primeira iteração
+        
+        tempo_bucket.append(1000*(end - start))
+
+        aux += 1
+
+    print('SORTED LIST (última execução):')
+    print(sorted_list)
+
+    # Calculando a média do tempo de execução considerando apenas as 4 últimas iterações
+    soma = sum(tempo_bucket)
+    media = soma / len(tempo_bucket)
+    desvio = calcular_desvio_padrao(tempo_bucket)
+
+    # Escrevendo os tempos de execução no arquivo CSV
+    with open("bucket_200000.csv", "w", newline="", encoding="utf-8") as aarquivo:
+        escritor = csv.writer(aarquivo)
+        escritor.writerow(["bucket_200000: "])  # Cabeçalho
+        for valor in tempo_bucket:
+            escritor.writerow([valor])
+        escritor.writerow(["Média do tempo de execução:", media])
+        escritor.writerow(["Desvio padrão do tempo de execução:", desvio])
+
+main1()
+main2()
+main3()
+main4()
+main5()
+main6()
+main7()
+main8()
